a95978b3a0c86e1b737d59cf63456a0ade9deb25
Muhammad Hafizoddin Bin Roslan <>
2023-10-09 23:10:37 +0800

    add

--- models/dataService.js
+++ models/dataService.js
@@ -2,12 +2,23 @@ const axios = require('axios');
 require('dotenv').config();
 
 exports.fetchFloodWarningData = async () => {
-    const response = await axios.get(process.env.FLOOD_API_URL);
-
-    return response.data;
+    try {
+        const response = await axios.get(process.env.FLOOD_API_URL);
+        console.log('Response Data:', response.data); // Log the response data
+        return response.data;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error; // Re-throw the error to handle it elsewhere if needed
+    }
 };
 
-exports.updateFloodDataInService = async (data, url) => {
-    const response = await axios.post(url, data);
-    return response.status;
+exports.updateFloodDataInService = async (url, data) => {
+    try {
+        const response = await axios.post(url, data);
+        console.log('Response Data:', response.data); // Log the response data
+        return response.status;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error; // Re-throw the error to handle it elsewhere if needed
+    }
 };
